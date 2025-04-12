6481185c82f99af167d1c74ace5960f6c88e0787
Lucas Sousa <lucasdevfront7@gmail.com>
2025-04-11 23:36:29 -0300

    Adição de funções para verificação de data e hora para não adicionar data inexistente

--- Turma 1/Equipe 4/mvp1.c
+++ Turma 1/Equipe 4/mvp1.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 #define TAM_FILA 10
 
@@ -20,6 +21,45 @@ void inifila(Fila* f) {
     f->primeiro = 0;
     f->ultimo = 0;
     f->qtdchamada = 0;
+}
+
+int validar_data(char dia[]) {
+    int dia_num, mes, ano;
+    if (sscanf(dia, "%2d/%2d/%4d", &dia_num, &mes, &ano) != 3) {
+        return 0;
+    }
+
+    if (mes < 1 || mes > 12) {
+        return 0;
+    }
+
+    int dias_no_mes[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+    if ((ano % 4 == 0 && (ano % 100 != 0 || ano % 400 == 0)) && mes == 2) {
+        dias_no_mes[1] = 29;
+    }
+
+    if (dia_num < 1 || dia_num > dias_no_mes[mes - 1]) {
+        return 0;
+    }
+
+    return 1;
+}
+
+int validar_hora(char hora[]) {
+    int hora_num, minuto;
+    if (sscanf(hora, "%2d:%2d", &hora_num, &minuto) != 2) {
+        return 0;
+    }
+
+    if (hora_num < 0 || hora_num > 23) {
+        return 0;
+    }
+
+    if (minuto < 0 || minuto > 59) {
+        return 0;
+    }
+
+    return 1;
 }
 
 int chamada_existe(Fila* f, int numero, char dia[], char hora[]) {
@@ -107,9 +147,17 @@ int main() {
 
                 printf("Digite o dia da chamada (formato dd/mm/aaaa): ");
                 scanf(" %10s", dia);
+                if (!validar_data(dia)) {
+                    printf("Data invalida! Tente novamente.\n");
+                    continue;
+                }
 
                 printf("Digite a hora da chamada (formato hh:mm): ");
                 scanf(" %5s", hora);
+                if (!validar_hora(hora)) {
+                    printf("Hora invalida! Tente novamente.\n");
+                    continue;
+                }
 
                 adicionar_chamada(&minha_fila, numero, dia, hora);
                 break;
